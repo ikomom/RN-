f48cf1d9b5789eeb29ac9d92cb495c2fdfcd1cef
ikonon <315793076@qq.com>
2018-10-03 13:13:42 +0800

    访问原生图片

--- sLearning/_7_image/Test.js
+++ sLearning/_7_image/Test.js
@@ -25,8 +25,22 @@ export default class Test extends React.Component {
     render() {
         return (
             <View>
-                <Image style={styles.image} source={require('./ballon.jpg')}/>
+                <Image
+                    style={styles.image}
+                    source={require('./ballon.jpg')}
+                    resizeMode={'center'}
+                />
+                <Image
+                    style={styles.image}
+                    source={{uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1538535617091&di=9583edcd9848d5a28ff7205c63a97cac&imgtype=0&src=http%3A%2F%2Fi1.hdslb.com%2Fbfs%2Farchive%2Fb05012339577bb690dce86e0b596adb3fa4057fd.jpg'}}
+                    resizeMode={'center'}
+                />
 
+                <Image
+                    style={styles.image}
+                    source={{uri:'ballon'}}
+                    resizeMode={'center'}
+                />
             </View>
         );
     }
@@ -34,8 +48,10 @@ export default class Test extends React.Component {
 
 const styles = StyleSheet.create({
     image:{
-        width:500,
-        height:100,
-        borderWidth: 1,
+        width:200,
+        height:150,
+        borderWidth: 5,
+        borderColor: 'red',
+        // tintColor: 'green'
     }
 });
